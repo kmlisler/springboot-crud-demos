bf0efcf9c96cf17c7d51753262a932f4053feb71
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-09-25 04:47:05 +0300

    adding rest controller method to find employee

--- src/main/java/com/kamilisler/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/kamilisler/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.kamilisler.cruddemo.entity.Employee;
 import com.kamilisler.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,14 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee==null){
+            throw new RuntimeException("Employee id not found : "+employeeId);
+        }
+        return theEmployee;
     }
 }
