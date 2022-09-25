c58fba701ab9c9c2e1204b922e806450555b7fae
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-09-25 14:42:09 +0300

    testing jpa dao implementation

--- src/main/java/com/kamilisler/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/kamilisler/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -2,6 +2,7 @@ package com.kamilisler.cruddemo.dao;
 
 import com.kamilisler.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -29,16 +30,25 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
 
     @Override
     public Employee findById(int Id) {
-        return null;
+        Employee theEmployee = entityManager.find(Employee.class,Id);
+        return theEmployee;
     }
 
     @Override
-    public void save(Employee employee) {
+    public void save( Employee theEmployee) {
+        // save or update the employee. if the id= 0 instert, otherwise update
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        // update with id from db.. so we can get generated id for save/insert
+        theEmployee.setId(dbEmployee.getId()); // useful for rest api
+
 
     }
 
     @Override
     public void deleteById(int Id) {
-
+        Query theQuery = entityManager.createQuery("delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId",Id);
+        theQuery.executeUpdate();
     }
 }
